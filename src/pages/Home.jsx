9f0cccf1521d967709fa93d48b282682a0526412
Alyfer Pedroso <alyferleandroaraujopedroso@gmail.com>
2023-12-27 10:57:52 -0300

    commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+
+import MovieCard from "../components/MovieCard";
+
+import "./MoviesGrid.css";
+
+const baseUrl = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+const Home = () => {
+  const [topMovies, setTopMovies] = useState([]);
+
+  const getTopRatedMovies = async (url) => {
+    const resp = await fetch(url);
+    const data = await resp.json();
+
+    setTopMovies(data.results);
+  };
+
+  useEffect(() => {
+    const topRatedUrl = `${baseUrl}top_rated?${apiKey}`;
+    getTopRatedMovies(topRatedUrl);
+  }, []);
+
+  return (
+    <div className="container">
+      <h2 className="title">Best Movies</h2>
+      <div className="movies-container">
+        {topMovies.length === 0 && <p>Loading...</p>}
+        {topMovies.length > 0 &&
+          topMovies.map((movie) => <MovieCard movie={movie} key={movie.id} />)}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/Search.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import MovieCard from "../components/MovieCard";
+
+const searchUrl = import.meta.env.VITE_SEARCH;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+import "./MoviesGrid.css";
+
+const Search = () => {
+  const [searchParams] = useSearchParams();
+
+  const [movies, setMovies] = useState([]);
+  const query = searchParams.get("q");
+
+  const getSearchedMovies = async (url) => {
+    const resp = await fetch(url);
+    const data = await resp.json();
+
+    setMovies(data.results);
+  };
+
+  useEffect(() => {
+    const searchWithQueryURL = `${searchUrl}?${apiKey}&query=${query}`;
+    getSearchedMovies(searchWithQueryURL);
+  }, [query]);
+
+  return (
+    <div className="container">
+      <h2 className="title">
+        Results for: <span className="query-text">{query}</span>
+      </h2>
+      <div className="movies-container">
+        {movies.length === 0 && <p>Loading...</p>}
+        {movies.length > 0 && movies.map((movie) => <MovieCard movie={movie} key={movie.id} />)}
+      </div>
+    </div>
+  );
+};
+
+export default Search;
